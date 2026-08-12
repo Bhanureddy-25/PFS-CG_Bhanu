d658c0dbcdc03418bbb11dbdeafa43b065d5e7b8
Bhanu Reddy Kilimi <148634694+Bhanureddy-25@users.noreply.github.com>
2026-08-12 10:25:17 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,78 @@
+#what is a program?
+#--> program is process of instructions given by the user or programmer
+#to planning what to do and how to do
+
+#Ex:
+
+user_input = 2000
+bal_ = 10000
+result = bal_ - user_input
+print(result)
+
+#procedural language
+#  is organized mainly on functions 
+# eg:'c'
+
+
+# Ex: 
+def cal_ad(a,b):
+ return a+b
+a = 7
+b = 9
+any_ = cal_ad(a,b)
+print(any_)
+
+
+# object oriented programming is only organized on objects and classes
+#Ex:python
+#ex:
+num = 7
+print(type(num))
+
+#what is python ??
+
+# python is a high level programming language 
+    # python takes care about memory allocation
+    #Ex:
+num = 9
+print(type(num))
+print(id(num)) 
+
+
+# interpreter language   
+    # line by line execution is followed in python that is the reason it is called interpreter lnaguage
+    #Ex:
+num = 9
+num_2 = 90
+Pint(num)
+print(type(num))
+print(id(num))
+
+
+# dynamically typed language 
+    # N0 need to mention the type of dat passing trough the variable
+    # Ex:
+num = 78
+num_ = 'python'
+nums = [1,2,3,4,5]
+all_ = (1,2,3,4,5)
+print(type(num))
+print(type(num_))
+print(type(nums))
+print(type(all_))
+
+
+# Why python? 
+    # 1. More libraries 
+    # 2. Cross-platform 
+    # 3. open source 
+    # 4. simple syntax 
+#Applications:
+    # 1. Web Development 
+    # 2. Artificial Intelligence 
+    # 3. machine learning
+    # 4. deep learning
+
+#it started in early days of 1980 and released in the year 1991 by Guido Van Russom 
+# And he picked this name from his most favorite series called "monty python"
+# The first version released in 1991 was 0.9.0 and now the latest version is 3.14
